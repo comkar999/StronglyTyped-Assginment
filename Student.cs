c94815e33c60561ae795c6bc8b3963c97e748ac8
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-26 19:02:33 -0500

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedAssignment.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedAssignment.Models;
+
+namespace StronglyTypedAssignment.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student s = new Student() { Name = "Mitchell", Address = "Sydney" };
+            return View(s);
+        }
+    }
+}
